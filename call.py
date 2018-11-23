1fb24c72440dfff66c6c08e94a1d9738a8166e71
RasithaGamage <g.rasitha@gmail.com>
2018-11-23 01:25:09 -1000

    Modified the code such that giving more accurate responses

--- call.py
+++ call.py
@@ -18,50 +18,69 @@ ser = serial.Serial(
         bytesize=serial.EIGHTBITS,
         timeout=None
 )
-
+                            
+sudoP = 'raspberry'
+command = 'gtts-cli "%s" -o /home/pi/Desktop/ims_temp.mp3'%message
+os.system('echo %s|sudo -S %s' % (sudoP,command))
+                            
 ser.write(('ATD'+phoneNum+';'+'\r\n').encode())
 time.sleep(5)
 
-flag = 0;
-result = "";
+result = ""
+
+dialing = 'false'
+answered = 'false'
+cut = 'false'
+
+carrierResponse = 'null'
 
 while(1):
         result = ser.readline()
         if result.strip()=='OK':
-            if flag <= 1:
-                
-                sudoP = 'raspberry'
-                command = 'gtts-cli "%s" -o /home/pi/Desktop/ims_temp.mp3'%message
-                os.system('echo %s|sudo -S %s' % (sudoP,command))
-                
-                time.sleep(10)
-                
-                os.system('echo %s|sudo -S %s' % (sudoPassword,'mpg321 -o alsa --loop 2 /home/pi/Desktop/ims_temp.mp3'))
-                os.system('echo %s|sudo -S %s' % (sudoPassword,'rm /home/pi/Desktop/ims_temp.mp3'))
- 
-
-                ser.write(('ATH\r\n').encode())
                 while(1):
                     result = ser.readline()
-                    if result.strip()=='OK':
-                        print("CALL ENDED")
-                        break
+                    
+                    checkVar1 = result.split(' ', 1 )
+                    
+                    if checkVar1[0]=='+CLCC:':
+                        checkVar2 = result.split(',', 3 )
+                        
+                        if checkVar2[2]=='3': #call dialing
+                            dialing = 'true'
+                            
+                        if checkVar2[2]=='0': #call answered
+                            answered = 'true'
+##                            subprocess.call('mpg321 -o alsa --loop 10 /home/pi/Desktop/ims_temp.mp3')
+                            p = subprocess.Popen(["mpg321","-o","alsa","--loop","5","/home/pi/Desktop/ims_temp.mp3"],stdout=subprocess.PIPE, stdin = subprocess.PIPE, stderr=subprocess.STDOUT)
+                            p.wait()
+                            os.system('echo %s|sudo -S %s' % (sudoP,'rm /home/pi/Desktop/ims_temp.mp3'))
+                            ser.write(('ATH\r\n').encode())
+                            callFinished='true'
+                            
+                        if checkVar2[2]=='6': #call cut
+                            cut='true'
+                            break
+                        
+                while(1):
+                    result = ser.readline()
                     
                     if result.strip()=='BUSY':
-                        print(result)
+                        carrierResponse='BUSY'
                         break
                     
                     if result.strip()=='NO ANSWER':
-                        print(result)
+                        carrierResponse='NO ANSWER'
                         break
                     
                     if result.strip()=='NO CARRIER':
-                        print(result)
+                        carrierResponse='NO CARRIER'
                         break
-                break
             
-            flag=flag+1;
-     
+         
+        if (dialing=='true'):
+            print(dialing+":"+answered+":"+cut+":"+carrierResponse)
+            break;
+                     
 #OK #BUSY #NO ANSWER #NO CARRIER     
 ser.flush()
 ser.close()
